2e1e7da17061537fcb7822799d6ebebb89bbf4f1
354079281@qq.com <huang0530>
2019-08-04 17:37:49 +0800

    log

--- admin/src/main/java/com/mod/admin/filter/ApiClientFilter.java
+++ admin/src/main/java/com/mod/admin/filter/ApiClientFilter.java
@@ -17,22 +17,22 @@ import org.slf4j.MDC;
  */
 @Activate(group = "admin-filter")
 @Slf4j
-public class ApiClientFilter implements Filter {
+public class ApiClientFilter implements Filter{
 
     @Override
-    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException{
         String traceId = MDC.get(RpcConstant.TRACE_KEY);
         SessionRequestContent.getRequest();
         String token = MDC.get(RpcConstant.USER_TOKEN);
-        if (StringUtil.hasLength(traceId)) {
+        if(StringUtil.hasLength(traceId)){
             RpcContext.getContext().setAttachment(RpcConstant.TRACE_KEY, traceId);
         }
         if(StringUtil.hasLength(token)){
-            RpcContext.getContext().setAttachment(RpcConstant.USER_TOKEN,token);
+            RpcContext.getContext().setAttachment(RpcConstant.USER_TOKEN, token);
         }
         String className = invoker.getInterface().getName();
         String param = GsonUtils.obj2Json(invocation.getArguments());
-        log.info("traceId- {} method:{}.{},request:{}", traceId,className , invocation.getMethodName(),param);
+        log.info("traceId- {} method:{}.{},request:{}", traceId, className, invocation.getMethodName(), param);
         return invoker.invoke(invocation);
     }
 

ec9f3ea9d6dba9ea2018c40f7c8cb539ad5cbcc5
huangdemo <huangdmeo.@shein.com>
2019-09-05 11:01:16 +0800

    message

---
+++ service/service-message/src/main/java/com/mod/message/service/IMesSysLogService.java
@@ -0,0 +1,16 @@
+package com.mod.message.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.mod.message.entity.po.MesSysLogPO;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author Mr.p
+ * @since 2019-09-05
+ */
+public interface IMesSysLogService extends IService<MesSysLogPO>{
+
+}

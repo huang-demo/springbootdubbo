e88add4bbc2000a15bf846a201cc6c82d1264c45
huangdemo <huangdmeo.@shein.com>
2019-09-05 11:00:23 +0800

    message

---
+++ provider-message/src/main/java/com/mod/message/dao/MesSysLogDao.java
@@ -0,0 +1,16 @@
+package com.mod.message.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mod.message.entity.po.MesSysLogPO;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author Mr.p
+ * @since 2019-09-05
+ */
+public interface MesSysLogDao extends BaseMapper<MesSysLogPO>{
+
+}
